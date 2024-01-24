62fbe0c3612c27241882de460570b9e4b29701a8
Rafa Takaki <rafalimatakak@hotmail.com>
2024-01-23 21:45:31 -0300

    Desafio Concluido

---
+++ Models/Classe.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio_classes_de_um_jogo.classe
+{
+    public class Classe 
+    {
+        private string nome { get; set; }
+        private int idade { get; set; }
+        private string tipo { get; set; }
+        private string ataque { get; set; }
+    
+        public Classe ( string nome, int idade, string tipo, string ataque)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.tipo = tipo;
+            this.ataque = ataque;
+        }
+
+        public void apresentar()
+        {
+            Console.WriteLine($"Seu nome é: {nome}, tem {idade} anos e está usando a classe {tipo}" );
+        }
+
+        public void atacar()
+        {
+            Console.WriteLine($"o {tipo} atacou usando {ataque}");
+        }
+
+        public void escolherClasse()
+        {
+            Console.WriteLine("Escolha sua Classe: ");
+
+            string opcao = string.Empty;
+            bool exibirMenu = true;
+
+
+            while (exibirMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a sua classe:");
+                Console.WriteLine("1 - Mago");
+                Console.WriteLine("2 - Guerreiro");
+                Console.WriteLine("3 - Monge");
+                Console.WriteLine("4 - Ninja");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                    tipo = "mago";
+                    ataque = "magia";
+                    exibirMenu = false;
+                    break;
+
+                    case "2":
+                    tipo = "guerreiro";
+                    ataque = "espada";
+                    exibirMenu = false;
+                    break;
+
+                    case "3":
+                    tipo = "monge";
+                    ataque = "artes marciais";
+                    exibirMenu = false;
+                    break;
+
+                    case "4":
+                    tipo = "ninja";
+                    ataque = "shuriken";
+                    exibirMenu = false;
+                    break;
+
+                    default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
+    
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using Desafio_classes_de_um_jogo.classe;
+
+Console.Clear();
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string nome = "";
+int idade = 0;
+string tipo = "";
+string ataque = "";
+
+
+Console.WriteLine("Digite seu nome: ");
+nome = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine("Digite dua Idade: ");
+idade = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+
+Classe conta = new Classe(nome, idade, tipo, ataque);
+
+conta.escolherClasse();
+
+string opcao = string.Empty;
+bool exibirMenu = true;
+
+// Realiza o loop do menu
+while (exibirMenu)
+{
+    Console.Clear();
+    Console.WriteLine("O que deseja fazer:");
+    Console.WriteLine("1 - Atacar");
+    Console.WriteLine("2 - Trocar de classe");
+    Console.WriteLine("3 - Sua conta");
+    Console.WriteLine("4 - Encerrar");
+
+    switch (Console.ReadLine())
+    {
+        case "1":
+            conta.atacar();
+            break;
+
+        case "2":
+            conta.escolherClasse();
+            break;
+
+        case "3":
+            conta.apresentar();
+            break;
+
+        case "4":
+            exibirMenu = false;
+            break;
+
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+
+    Console.WriteLine("Pressione uma tecla para continuar");
+    Console.ReadLine();
+}
+
+Console.WriteLine("O programa se encerrou");
+
+
+
+
+
+
+
+
+
